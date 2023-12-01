ce8b6e7585818d23e62da44e80d5712c990499e2
baoganglin <baoganglin@joyy.com>
2023-12-01 22:02:57 +0800

    dp 413

--- leetcode_101_by_python3/ch07_DP/leetcode413.py
+++ leetcode_101_by_python3/ch07_DP/leetcode413.py
@@ -1,24 +1,29 @@
 from typing import List
 
 
-def number_of_arithmetic_slices(nums: List[int]) -> int:
-    nums_len = len(nums)
-    if nums_len <= 2:
-        return 0
-    cnt = 0
-    d, t = nums[1] - nums[0], 0
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        length = 2
+        cnt = 0
+        low, high = 0, 2
+        while high < len(nums):
+            if nums[high] - nums[high-1] == nums[high - 1] - nums[low]:
+                length += 1
+                cnt += length - 2
+            else:
+                length = 2
+            low += 1
+            high = low + 2
+        return cnt
 
-    for i in range(2, nums_len):
-        if nums[i] - nums[i-1] == d:
-            t += 1
-        else:
-            t = 0
-            d = nums[i] - nums[i-1]
-        cnt += t
-    return cnt
+
+
 
 
 if __name__ == '__main__':
-    nus = [1, 2, 3, 4]
-    ret = number_of_arithmetic_slices(nus)
+    nus = [1, 2, 3, 4, 6, 8]
+    s = Solution()
+    ret = s.numberOfArithmeticSlices(nus)
     print(ret)
+    # ret = number_of_arithmetic_slices(nus)
+    # print(ret)
