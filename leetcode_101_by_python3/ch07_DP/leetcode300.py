6271d2cf13f75d89178d95672ce14b71013fc5a1
baoganglin <baoganglin@joyy.com>
2023-12-07 22:08:10 +0800

    leetcode300

---
+++ leetcode_101_by_python3/ch07_DP/leetcode300.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = {}
+        dp[1] = nums[0]
+        for num in nums[1:]:
+            flag = True
+            for i in range(1, len(dp)+1):
+                if num <= dp[i]:
+                    dp[i] = num
+                    flag = False
+                    break
+            if flag:
+                dp[i+1] = num
+        return len(dp)
+
+
+if __name__ == '__main__':
+    nums = [4, 10, 4, 3, 8, 9]
+    s = Solution()
+    s.lengthOfLIS(nums)
